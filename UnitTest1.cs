57510ba0e05b448746e0483d95da7c970593a00b
Naga Venkat N Kolla <mailtokolla@gmail.com>
2024-01-31 10:54:40 +0530

    New MS Test Framework Project

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,24 +1,22 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 namespace MSTestProject1
 {
-    using OpenQA.Selenium;
-    using OpenQA.Selenium.Chrome;
-
         [TestClass]
         public class UnitTest1
         {
-            static IWebDriver driver;
-
             [TestMethod]
             public void TestWebSiteText()
             {
                 string assignedtext = "Welcome Viaan Kolla with your parents!!!";
                 System.Console.WriteLine("Hello Guys");
-                driver = new ChromeDriver();
+                System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", "chromedriver.exe");
+                IWebDriver driver = new ChromeDriver("C:\\Projects\\webapps\\ChromeBrowserTesting\\bin\\Debug\\net6.0"); //<-Add your path
                 driver.Navigate().GoToUrl("https://dotnetcorewebapp11.azurewebsites.net/");
                 String text = driver.FindElement(By.CssSelector("h1")).Text;
-                System.Console.WriteLine(text);
                 Assert.IsTrue(text.Equals(assignedtext));
-                //Assert.Equals(text, assignedtext);
                 driver.Close();
             }
         }
